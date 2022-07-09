49d592364e3c93ae0ad0b5258071e539303247a6
Artimedias <brettroper22@gmail.com>
2022-07-09 00:35:14 -0700

    Code now pases the first test, the caulculations for the pastries are now done

--- Store.Tests/modelTests/CookieTests.cs
+++ Store.Tests/modelTests/CookieTests.cs
@@ -11,9 +11,11 @@ namespace cookieTesting
         [TestMethod]
         public void screaming()
         {
-            int a = 8;
             cookieThing newThing = new cookieThing();
-            Assert.AreEqual(newThing.monster(4), a);
+            Assert.AreEqual(7, newThing.monster(4, 0));
+            Assert.AreEqual(9, newThing.monster(5, 0));
+            Assert.AreEqual(10, newThing.monster(6, 0));
+            Assert.AreEqual(32, newThing.monster(19, 0));
         }
     }
 }
